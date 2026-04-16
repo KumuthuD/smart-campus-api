594885237cbef8b307c185984dcaa6f1df6c6a66
KumuthuD <kumuthudahanayake@gmail.com>
2026-04-16 10:23:13 +0530

    chore: add package-info documentation files for exception and filter packages

---
+++ src/main/java/com/smartcampus/exception/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package is reserved for JAX-RS exception mappers.
+ */
+package com.smartcampus.exception;
---
+++ src/main/java/com/smartcampus/filter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package is reserved for JAX-RS request/response filters.
+ */
+package com.smartcampus.filter;
